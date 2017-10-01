d7c626a3cd28987c80f4db05d373edeb3facc8f5
PRAVEENKUMAR <PRAVEENKUMAR@Praveen>
2017-10-01 13:50:21 -0400

    Updated code

---
+++ src/restAPIPackage/restAPITests.java
@@ -0,0 +1,26 @@
+package restAPIPackage;
+
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import java.util.*;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
+
+public class restAPITests
+{
+	@Test
+	public void sampleTest() {
+		Response resp = get("http://services.groupkt.com/country/get/all");
+		System.out.println("resp : "+resp.getStatusCode());
+		
+		String json = resp.asString();
+		JsonPath jsonpath = new JsonPath (json);
+		
+		System.out.println("jsonpath : "+jsonpath.get("ResResponse").toString());
+		
+		//given().get("http://services.groupkt.com/country/get/iso2code/IN").then().statusCode(200).log().all();
+	}
+}
